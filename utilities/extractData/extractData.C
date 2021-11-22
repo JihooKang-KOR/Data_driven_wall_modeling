a8d357ed4438a455c3c684c16f8ecb6b6dff19e0
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2021-11-23 00:34:31 +0100

    Extracting criterion changed to yPlus <= 400

--- utilities/extractData/extractData.C
+++ utilities/extractData/extractData.C
@@ -46,6 +46,7 @@ int main(int argc, char *argv[])
     #include "createTime.H"
     #include "createMesh.H"    
     
+    #include "createFields.H"
     #include "saveData_yCoord.H"
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
